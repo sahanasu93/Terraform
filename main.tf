815794cdf3e9e0933a0ac3b42c2f3e147f19a9c4
sahanasu93 <103564618+sahanasu93@users.noreply.github.com>
2022-07-11 21:04:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# Configure the Microsoft Azure Provider
+# Configure the Microsoft Azure Provider example
 provider "azurerm" {
     # The "feature" block is required for AzureRM provider 2.x. 
     # If you're using version 1.x, the "features" block is not allowed.
